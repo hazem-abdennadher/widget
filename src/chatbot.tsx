d97beac6230ed783f17e77853c5a663135e9b723
hazem <hazem0abdennadher@gmail.com>
2023-10-13 19:49:50 +0100

    fixes

--- src/chatbot.tsx
+++ src/chatbot.tsx
@@ -1,13 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Chatbot = () => {
-  const [open, setOpen] = useState(false);
+const Chatbot: React.FC = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div
-      className="fixed w-16 h-16 bg-blue-600 bottom-10 right-10 rounded-full border-red-600 z-50"
-      onClick={() => setOpen((s) => !s)}
+      className={`fixed bottom-4 right-4 p-2 bg-blue-500 rounded-full cursor-pointer ${
+        expanded ? "h-64 w-64" : "h-12 w-12"
+      }`}
+      onClick={handleToggle}
     >
-      {open ? "Open" : "Closed"}
+      {expanded ? (
+        <div className="bg-white p-4 h-full">
+          <div className="flex justify-between mb-2">
+            <div className="font-bold text-lg">Chatbot</div>
+            <div
+              className="cursor-pointer text-xl"
+              onClick={() => setExpanded(false)}
+            >
+              X
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="text-white text-xl text-center">Chat</div>
+      )}
     </div>
   );
 };
