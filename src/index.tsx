32ad9c4ab50a771aac8121a05c475fbcd8bbc48f
hazem <hazem0abdennadher@gmail.com>
2023-10-13 19:23:06 +0100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Chatbot from "./chatbot";
 document.addEventListener("DOMContentLoaded", () => {
+  const chabotName = document.querySelector('script[data-name="chatbot"]');
+  console.log(chabotName);
   const div = document.createElement("div");
   document.body.appendChild(div);
   const root = createRoot(div);
